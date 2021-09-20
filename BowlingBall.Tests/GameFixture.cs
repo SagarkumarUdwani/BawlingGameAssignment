8e527a2719b98480d97a68f1f2b86562a217a7e6
SagarkumarUdwani <91044533+SagarkumarUdwani@users.noreply.github.com>
2021-09-20 10:20:20 +0530

    Add files via upload

---
+++ BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class GameFixture
+    {
+        private Game game;
+
+        [TestInitialize]
+        public void Initialized()
+        {
+            game = new Game();
+        }
+
+        [TestMethod]
+        public void Gutter_game_score_should_be_zero_test()
+        {
+            //var game = new Game();
+            Roll(game, 0, 20);
+            Assert.AreEqual(0, game.GetScore());
+        }
+
+        [TestMethod]
+        public void Score_All_Rolls_Should_Be_One_test()
+        {
+            Roll(game, 1, 20);
+            Assert.AreEqual(20, game.GetScore());
+        }
+
+        [TestMethod]
+        public void CanRollSpare_test()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+            Roll(game, 0, 17);
+            Assert.AreEqual(16, game.GetScore());
+        }
+
+        [TestMethod]
+        public void CanRollStrike_test()
+        {
+            game.Roll(10);
+            game.Roll(3);
+            game.Roll(4);
+            Roll(game, 0, 16);
+            Assert.AreEqual(24, game.GetScore());
+        }
+
+        [TestMethod]
+        public void CanRollPerfectGame_test()
+        {
+            Roll(game, 10, 12);
+            Assert.AreEqual(300, game.GetScore());
+        }
+
+        [TestMethod]
+        public void CanRollAssignmentGame_test()
+        {
+            game.Roll(10); // Frame 1
+            game.Roll(9);  // Frame 2
+            game.Roll(1);  // Frame 2
+            game.Roll(5);  // Frame 3
+            game.Roll(5);  // Frame 3
+            game.Roll(7);  // Frame 4
+            game.Roll(2);  // Frame 4
+            game.Roll(10);  // Frame 5
+            game.Roll(10);  // Frame 6
+            game.Roll(10);  // Frame 7
+            game.Roll(9);  // Frame 8
+            game.Roll(0);  // Frame 8
+            game.Roll(8);  // Frame 9
+            game.Roll(2);  // Frame 9
+            game.Roll(9);  // Frame 10
+            game.Roll(1);  // Frame 10
+            game.Roll(10);  // Frame 10
+            Assert.AreEqual(187, game.GetScore());
+        }
+
+        private void Roll(Game game, int pins, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+    }
+}
---
+++ BowlingBall/Game.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBall
+{
+    interface IGame
+    {
+        int GetScore();
+    }
+
+    public abstract class BowlingGame
+    {
+        protected const int _maxScore = 300;
+
+        protected const int _minPins = 0;
+
+        protected const int _maxPins = 10;
+
+        public abstract void Roll(int ball);
+    }
+
+    public class Game : BowlingGame, IGame
+    {
+        /*
+        Possible Solutions but can cause an issue
+        Approach 1 : Variable Shuffling
+        Approach 2 : Using Array of Array
+        Approach 3 : List of Dictionary
+        */
+
+        //Approach 4  : Array
+        private int[] rolls = null;
+
+        //Hold recent roll index
+        int currentRoll = 0;
+
+        public Game()
+        {
+            //Maximum 10 Frame * 2 Balls in each frame + 1 for last frame
+            rolls = new int[21];
+        }
+
+        public override void Roll(int pins)
+        {
+            //Adding each balls score to rolls
+            if (pins < _minPins)
+                pins = _minPins;
+            else if (pins > _maxPins)
+                pins = _maxPins;
+
+            try
+            {
+                rolls[currentRoll++] = pins;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                //Warning : Maximum 21 balls can be played
+            }
+        }
+
+        public int GetScore()
+        {
+            // Returns the final score of the game.
+            int score = 0;
+            int rollIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(rollIndex))
+                {
+                    score += CalculateSpecialScore(rollIndex);
+                    rollIndex++;
+                }
+                else if (IsSpare(rollIndex))
+                {
+                    score += CalculateSpecialScore(rollIndex);
+                    rollIndex += 2;
+                }
+                else
+                {
+                    score += CalculateStandardScore(rollIndex);
+                    rollIndex += 2;
+                }
+            }
+
+            return (score > _maxScore ? _maxScore : score) ;
+        }
+
+        private bool IsSpare(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+        }
+
+        private bool IsStrike(int rollIndex)
+        {
+            return rolls[rollIndex] == 10;
+        }
+
+        private int CalculateStandardScore(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1];
+        }
+
+        private int CalculateSpecialScore(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+        }
+
+        /*
+        private int CalculateSpareScore(int rollIndex)
+        {
+            return 10 + rolls[rollIndex + 2];
+        }
+
+        private int CalculateStrikeScore(int rollIndex)
+        {
+            return 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+        }
+        */
+    }
+}
